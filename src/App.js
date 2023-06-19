07b90e08d3d48605e433ad9464fa3591dc6192d1
Dzmitry Piachynski <pechinskiy.dima@gmail.com>
2023-06-19 18:18:06 +0200

    Animated display of the modal window

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [modal, setModal] = useState(false)
+
+    return (
+        <div className="App">
+            <button className="open-modal-btn" onClick={() => setModal(true)}>✨ Open window</button>
+            <div className={`overlay animated ${modal ? 'show' : ''}`}>
+                <div className="modal">
+                    <svg height="200" viewBox="0 0 200 200" width="200"
+                         onClick={() => setModal(false)}>
+                        <title/>
+                        <path
+                            d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
+                    </svg>
+                    <img src="https://media.giphy.com/media/GeimqsH0TLDt4tScGw/giphy.gif"/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
